f16c9ebe370310e731ebbb3486dc0c61101c8581
Geonho Lim <dlarjsgh67@naver.com>
2024-05-13 21:20:48 +0900

    dd

--- app/src/main/java/com/sahmyook/ugs/MainActivity.java
+++ app/src/main/java/com/sahmyook/ugs/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
-        //테스트
+        //제발 돼라
         // 계정 탈퇴 할때는
         // mFireBaseAuth.getCurrentUser().delete();
     }
